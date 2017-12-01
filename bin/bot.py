91257a13f3edfa1f7c27120a8b62748f7bf0f964
ilya_dzeraziak <ilyadzeraziak@gmail.com>
2017-12-01 21:27:51 +0300

    added json dataloader

--- bin/bot.py
+++ bin/bot.py
@@ -1,21 +1,20 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
+class Bot:
+    chatbot = ChatBot("ilya", read_only = true)
+    
+    class Data:
+        TrainigDataJson = [
+            
+        ]
+        
 
-chatbot = ChatBot("ilya")
 
-conversation = [
-    "Hello",
-    "Hi there!",
-    "How are you doing?",
-    "That is good to hear",
-    "Thank you.",
-    "You're welcome."
-]
+        chatbot.set_trainer(ListTrainer)
+        chatbot.train(conversation)
+        message = input('Your message:\n')
+        response = chatbot.get_response(message);
+        print(response); 
 
-chatbot.set_trainer(ListTrainer)
-chatbot.train(conversation)
-
-message = input('Your message:\n')
-response = chatbot.get_response(message);
-print(response); 
+Bot.Data.
