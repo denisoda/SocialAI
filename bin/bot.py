d027ecf92d9335d7a6307993611d01c374d34d0d
Ilya Dzeraziak <ilya.by@bk.ru>
2017-12-05 23:41:47 +0300

    Ref changes

--- bin/bot.py
+++ bin/bot.py
@@ -1,20 +1,7 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
+import json
 
 class Bot:
-    chatbot = ChatBot("ilya", read_only = true)
-    
-    class Data:
-        TrainigDataJson = [
-            
-        ]
-        
+    chatbot = ChatBot("Ilya")
 
-
-        chatbot.set_trainer(ListTrainer)
-        chatbot.train(conversation)
-        message = input('Your message:\n')
-        response = chatbot.get_response(message);
-        print(response); 
-
-Bot.Data.
